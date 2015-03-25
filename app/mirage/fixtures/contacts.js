72445149b9a7ebce3af3eb74cb1d07aac81f6e33
Tal Magen <talm@dudamobile.com>
2015-03-25 11:33:36 +0200

    init ember-cli-boilerplate

---
+++ app/mirage/fixtures/contacts.js
@@ -0,0 +1,32 @@
+/*
+  This is an example. This data will be added to the db
+  under the `contacts` key.
+
+  Create more files in this directory to add more data.
+*/
+export default [
+  {
+    id: 1,
+    name: 'Zelda'
+  },
+  {
+    id: 2,
+    name: 'Link'
+  },
+  {
+    id: 3,
+    name: 'Oz'
+  },
+  {
+    id: 4,
+    name: 'Mendy'
+  },
+  {
+    id: 5,
+    name: 'Josef'
+  },
+  {
+    id: 6,
+    name: 'Gev'
+  }
+];
